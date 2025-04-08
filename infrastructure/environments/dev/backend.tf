e2984fe2001c67965fedf2da907ea09a48f90b6f
Amanpandey7 <Aman711pandey@gmail.com>
2025-04-09 04:27:14 +0530

    added bucket

--- infrastructure/environments/dev/backend.tf
+++ infrastructure/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "your-terraform-state-bucket"
+    bucket = "json-formatter-dev-terraform-state"
     key    = "json-formatter/dev/terraform.tfstate"
     region = "us-east-1"
   }
